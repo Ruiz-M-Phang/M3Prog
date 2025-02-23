1bcc52eea5f7b3e55c01b85c9b63557c1581e414
Ruiz-M-Phang <Ruiz.M.Phang@gmail.com>
2025-02-23 01:35:35 +0100

    variabelen

--- m3prog_project/public/01/phphtml-var.php
+++ m3prog_project/public/01/phphtml-var.php
@@ -3,23 +3,28 @@
 $title = "Mijn mooie website";
 $datum = date('d-m-Y');
 $tijd = date("H:i");
-$favorieteGame = "Super metroid";
+$favorieteGame = "Super metroid (wie weet, hangt van af. Ok het antwoord is ja.)";
 $mooisteGetal = 42;
+$favorieteShow = "Corpse Party";
+$leuksteStad = "Anime stad";
 
-echo $title;
-echo $datum;
-echo $tijd;
-echo $favorieteGame;
-echo $mooisteGetal;
+
 ?>
 <!doctype html>
 <html lang="nl">
     <head>
-        <title><?php echo "Titel van de pagina" ?></title>
+        <title><?= $title ?></title>
     </head>
     <body>
-        <h2>De datum vandaag is: <?php echo date('d-m-Y')?>, en de tijd is <?php echo date("H:i")?> uur.
+        <h2>De datum vandaag is: <?= $datum?>, en de tijd is <?= $tijd?> uur.
         
-        <p><h2>Mijn favoriete game</h2><?php echo "Super metroid";?></p>
+        <p><h2>Mijn favoriete game</h2><?= $favorieteGame;?></p>
+        <br>
+        <p><h2>Mooiste getal</h2><?= $mooisteGetal;?></p>
+        <br>
+        <p><h2>Favoriete show</h2><?= $favorieteShow;?></p>
+        <br>
+        <p><h2>Leukste stad</h2><?= $leuksteStad;?></p>
+
     </body>
 </html>
