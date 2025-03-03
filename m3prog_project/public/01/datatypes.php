73618222dd634b728cddf83fb63236af4c8ba16f
Ruiz-M-Phang <Ruiz.M.Phang@gmail.com>
2025-03-03 23:18:48 +0100

    datatypes

---
+++ m3prog_project/public/01/datatypes.php
@@ -0,0 +1,59 @@
+<?php
+
+//Code die ik kan kopieëren :)
+$nogLeeg = null;
+
+//Hier type ik relevante code
+$huisnummer = "Contactweg 36 1014 AN Amsterdam";
+$naam = "Mediacollege Amsterdam";
+$postbus = "Postbus 67003";
+$adres = "1060 JA Amsterdam";
+$telefoonnummer = "020 850 9500";
+$geslaagd = "niet geslaagd";
+$prijs = 0.96;
+$liter = 1;
+
+echo $huisnummer;
+echo "<br>";
+echo "<br>";
+echo $naam;
+echo "<br>";
+echo $postbus;
+echo "<br>";
+echo $adres;
+echo "<br>";
+echo "<br>";
+echo $telefoonnummer;
+echo "<br>";
+echo "<br>";
+echo "Je bent {$geslaagd} voor je rijbewijs";
+echo "<br>";
+echo "<br>";
+echo "Euro 95 benzine prijs is €{$prijs} per liter";
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+<script>
+    setTimeout(() => {
+        console.log("DOXXING THIS LOCATION IN 10 SECONDS")
+    }, 5000)
+
+    setTimeout(() => {
+        console.log("THE LOCATION HAS BEEN DOXXED");
+    }, 15000)
+    setTimeout(() => {
+        console.log("Het is maar een grap");
+    }, 15500)
+</script>
+</html>
---
+++ m3prog_project/public/01/showtypes.php
@@ -0,0 +1,45 @@
+<?php
+
+//Code die ik kan kopieëren :)
+$nogLeeg = null;
+
+//Hier type ik relevante code
+$nummer = 89;
+$salaris = 0.5;
+$zin = "hello zin";
+$waarOfNiet = false;
+
+$rijtje = [1,2,3,4,5];
+
+$getType = gettype($nummer);
+
+$x = asin(3);
+
+echo gettype($nummer);
+echo "<br>";
+echo gettype($salaris);
+echo "<br>";
+echo gettype($zin);
+echo "<br>";
+echo gettype($waarOfNiet);
+echo "<br>";
+echo gettype($rijtje);
+echo "<br>";
+echo gettype($nogLeeg);
+echo "<br>";
+echo gettype($getType);
+echo "<br>";
+echo gettype($x);
+
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
