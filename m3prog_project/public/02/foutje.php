3077870e8f6347942505ecaae73bd2b07431f941
Ruiz-M-Phang <Ruiz.M.Phang@gmail.com>
2025-03-03 23:40:42 +0100

    foutje

---
+++ m3prog_project/public/02/foutje.php
@@ -0,0 +1,18 @@
+<?php
+    echo "Dit is geen foute code omdat de quotes wel dezelfde zijn";
+    echo "<br>";
+    echo "<br>";
+    echo 100/10; // Hier wordt door nul gedeeld, dat is ook een fout!
+
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ m3prog_project/public/02/foutje2.php
@@ -0,0 +1,18 @@
+<?php
+    echo "dit is goed toch?";
+    echo "<br>";
+    echo "<br>";
+    echo "hoop ik?";
+
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
