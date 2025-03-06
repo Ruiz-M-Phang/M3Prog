8b211ddc5ba0d81b8d100117f46e30f5caac98ec
Ruiz-M-Phang <Ruiz.M.Phang@gmail.com>
2025-03-06 10:32:55 +0100

    if

--- m3prog_project/public/03/if.php
+++ m3prog_project/public/03/if.php
@@ -1,21 +1,46 @@
 <?php
     $eenBoolVariable =false;
-    $aantalOpVoorraad = 5;
-    $welOpvoorraad= $aantalOpVoorraad > 10; // Boven de tien is if
+    $aantalOpVoorraad = 0;
+    $opvoorraad= $aantalOpVoorraad > 0; //Bedoel je dit $aantalOpVoorraad <= 0
     // $opvoorraad= $aantalOpVoorraad == 30;
-    $opvoorraad= 0 < $aantalOpVoorraad; // onder de tien is else if
     // $nietOpvoorraad = true;
 
-    if($welOpvoorraad){
-        echo "wel op voorraad";
-    }
-    else if($opvoorraad){
+    // if($opvoorraad == false)
+    if($aantalOpVoorraad > 0){ //Bedoel je dit $aantalOpVoorraad <= 0
         echo "niet op voorraad";
+    }
+    else{
+        echo "Artiekel is op voorraad";
+    }
+
+    echo "<br>";
+    echo "<br>";
+
+    // $welOpvoorraad= $aantalOpVoorraad > 10; // Boven de tien is if
+    // $opvoorraad= 0 < $aantalOpVoorraad; // onder de tien is else if
+
+    // if($welOpvoorraad){
+    //     echo "wel op voorraad";
+    // }
+    // else if($opvoorraad){
+    //     echo "niet op voorraad";
+    // }
+
+    echo "<br>";
+    echo "<br>";
+
+    if(false)  //() komt uit op false
+    {
+        // dit wordt NIET uitgevoerd
+    }
+    else
+    {
+        //nu gaat deze code uitgevoerd worden
     }
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
