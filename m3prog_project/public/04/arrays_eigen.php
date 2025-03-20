6f4ddedc9a2aca2f23793db9ccc822e60e25cf5f
Ruiz-M-Phang <Ruiz.M.Phang@gmail.com>
2025-03-20 10:40:53 +0100

    arrays_eigen

---
+++ m3prog_project/public/04/arrays_eigen.php
@@ -0,0 +1,22 @@
+<?php
+
+$games = ["Fate/Grand Order",
+ "Genshin Impact",
+  "Azur Lane",
+   "Zenless Zone Zero",
+    "Honkai Starrail"];
+
+    // Print de array in tekst formaat
+    print_r($games);
+
+    // Geeft wat extra info over het soort data dat er in zit
+    var_dump($games); 
+
+    echo "<br>";
+    echo "<br>";
+    echo $games[2];
+    echo "<br>";
+    echo $games[4];
+    echo "<br>";
+    echo $namen;
+    echo "<br>";
