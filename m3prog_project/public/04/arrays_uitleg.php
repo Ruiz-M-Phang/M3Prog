86b0b4344e440e86ec1d744b53e939ee5be97325
Ruiz-M-Phang <Ruiz.M.Phang@gmail.com>
2025-03-20 10:15:14 +0100

    arrays_index

--- m3prog_project/public/04/arrays_uitleg.php
+++ m3prog_project/public/04/arrays_uitleg.php
@@ -17,3 +17,20 @@ $nummers = [1,2,3];
 array_push($nummers, 4, 5, 6, 69, 666);
 
 print_r($nummers);
+
+echo "<br>";
+echo $nummers[1];
+echo "<br>";
+echo $nummers[0];
+echo "<br>";
+echo $nummers[4];
+echo "<br>";
+echo $nummers[7];
+echo "<br>";
+echo $dagen[4];
+echo "<br>";
+echo $dagen[2];
+echo "<br>";
+echo $dagen[6];
+echo "<br>";
+echo $dagen[1];
