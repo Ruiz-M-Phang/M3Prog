0b2a4a625a3fcc3655ed0b73af4ec5685549ae57
Ruiz-M-Phang <Ruiz.M.Phang@gmail.com>
2025-03-27 10:45:18 +0100

    phpfunctions.php

--- m3prog_project/public/05/phpfunctions.php
+++ m3prog_project/public/05/phpfunctions.php
@@ -4,15 +4,28 @@ $hoeLangIsDitWoord = "scrabbleWoord";
 
 $woordLengte = strlen($hoeLangIsDitWoord);
 
-// Vraag 1: De type die door de functie strlen wordt geaccepteerd is string
+// Vraag 1.1: De type die door de functie strlen wordt geaccepteerd is string
 
-// Vraag 2: Het returnd de lengte van de string in nummers en begint vanaf 1 dan bij 0
+// Vraag 1.2: Het returnd de lengte van de string in nummers en begint vanaf 1 dan bij 0
 
-// Vraag 3: Het getal is voor php een byte
+// Vraag 1.3: Het getal is voor php een byte
 
 $langeWoordPuntCom = "https://mywaifulist.moe/popular"; // Niet op de link klikken
 
-$geefWoordLengte = strlen($langeWoordPuntCom)
+$geefWoordLengte = strlen($langeWoordPuntCom);
+
+$tweeMacht2 = pow(2,2);
+
+pow(2,10);
+
+$derdeMacht = pow(3,4);
+
+// https://www.php.net/manual/en/function.strtoupper.php
+$naam = "mario";
+
+$naamGroot = strtoupper($naam);
+
+$naamKlein = strtolower($naamGroot);
 
 ?>
 <!DOCTYPE html>
@@ -24,12 +37,32 @@ $geefWoordLengte = strlen($langeWoordPuntCom)
 </head>
 <body>
     <?php
-    // Vraag 4: Hier zie je dat ik de functie strlen al 2 keer heb aan geroepen.
+    // Vraag 1.4: Hier zie je dat ik de functie strlen al 2 keer heb aan geroepen.
     print($woordLengte);
 
     echo "<br>";
 
     print($geefWoordLengte);
+
+    echo "<br>";
+
+    print($tweeMacht2);
+
+    echo "<br>";
+
+    print(pow(2,10));
+
+    print("<br>");
+    print(pow(3,4)); // Vraag 2.1: We geven het antwoord door met behulp van de print, pow functie en integers tussen de haakjes van pow zetten
+
+    print("<br>");
+    print($naamGroot);
+
+    print("<br>");
+    print($naamKlein);
+
+    print("<br>");
+    print($naam);
 
     ?>
 </body>
