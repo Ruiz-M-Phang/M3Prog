433df039b419b2bb57dea4cbbafc16b88701f7cc
Ruiz-M-Phang <Ruiz.M.Phang@gmail.com>
2025-03-28 19:50:05 +0100

    gebruikanderefile.php

---
+++ m3prog_project/public/05/gebruikanderefile.php
@@ -0,0 +1,28 @@
+<?php
+
+//The following math functions are added
+// add()
+// subtract()
+// divide()
+// multiply()
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    include_once "returncalc.php";
+
+    // echo "<br>";
+
+    multiply(5, 10);
+
+    ?>
+</body>
+</html>
