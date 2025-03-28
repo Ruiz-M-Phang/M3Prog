4dfa5ae625b50b86bad8ebcfd08bdef059a3c37f
Ruiz-M-Phang <Ruiz.M.Phang@gmail.com>
2025-03-28 10:07:50 +0100

    nagemaakt.php

--- m3prog_project/public/05/nagemaakt.php
+++ m3prog_project/public/05/nagemaakt.php
@@ -13,7 +13,7 @@ function mijnPrint($printText)
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
