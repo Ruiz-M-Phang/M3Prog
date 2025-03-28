7caeb44d7022372fe76f089e8187e9ce1600db09
Ruiz-M-Phang <Ruiz.M.Phang@gmail.com>
2025-03-28 19:21:16 +0100

    returns.php

--- m3prog_project/public/05/returns.php
+++ m3prog_project/public/05/returns.php
@@ -1,13 +1,57 @@
 <?php
 
-$mijnFunctionResultaat = 1;
+// $mijnFunctionBerekening = pow(25, 10);
 
-function mijnFunction($mijnFunctionResultaat)
+// function mijnBereking($mijnFunctionBerekening)
+// {
+//     print($mijnFunctionBerekening);
+
+//     return pow(25, 10);
+// }
+
+function mijnFunction()
 {
-    $mijnFunctionResultaat = 1;
-    print($mijnFunctionResultaat);
 
     return 1;
+}
+
+$mijnFunctionResultaat = mijnFunction();
+echo $mijnFunctionResultaat;
+
+function returnVariable()
+{
+    $result = rand();
+    print($result);
+    return $result;
+}
+
+function randomPower()
+{
+    $randomCalcultion = rand();
+    $powerCalcultion = pow(
+        $randomCalcultion, 
+        $randomCalcultion);
+    print($powerCalcultion);
+    return $powerCalcultion;
+}
+
+function randomIntdiv()
+{
+    $randomCalcultion = rand();
+    $intdivCalcultion = intdiv(
+        $randomCalcultion, 
+        $randomCalcultion);
+    print($intdivCalcultion);
+    return $intdivCalcultion;
+}
+
+function randomSqrt()
+{
+    $randomCalcultion = rand();
+    $sqrtCalcultion = sqrt(
+        $randomCalcultion);
+    print($sqrtCalcultion);
+    return $sqrtCalcultion;
 }
 
 ?>
@@ -21,7 +65,23 @@ function mijnFunction($mijnFunctionResultaat)
 <body>
     <?php
 
-    mijnFunction($mijnFunctionResultaat);
+    echo "<br>";
+    echo "<br>";
+    returnVariable();
+    echo "<br>";
+    returnVariable();
+    echo "<br>";
+    returnVariable();
+    echo "<br>";
+    echo "<br>";
+    randomPower();
+    echo "<br>";
+    echo "<br>";
+    randomIntdiv();
+    echo "<br>";
+    echo "<br>";
+    randomSqrt();
+    // mijnBereking($mijnFunctionBerekening);
 
     ?>
 </body>
