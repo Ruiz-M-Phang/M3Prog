f92d1c5d2c0c256f432d21198f21b48fdc4c752b
Ruiz-M-Phang <Ruiz.M.Phang@gmail.com>
2025-04-13 21:43:40 +0200

    Thank_you_for_learning_me_php

--- m3prog_project/public/07/array_assoc.php
+++ m3prog_project/public/07/array_assoc.php
@@ -1,13 +1,13 @@
 <?php
 
 $hetWeer = [
-    "ma" => 12,
-    "di" => 5,
-    "wo" => 7,
-    "do" => 13,
-    "vr" => 16,
-    "za" => 19,
-    "zo" => 18];
+    "maandag" => 12,
+    "dinsdag" => 5,
+    "woensdag" => 7,
+    "donderdag" => 13,
+    "vrijdag" => 16,
+    "zaterdag" => 19,
+    "zondag" => 18];
 
 ?>
 <!DOCTYPE html>
@@ -16,16 +16,16 @@ $hetWeer = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/assets/css/array_assocStyle.css">
-    <title>array assoc</title>
+    <title>Het weer</title>
 </head>
 <body>
     <table>
     <?php
-    print_r($hetWeer);
+    // print_r($hetWeer);
 
     echo "<tr>";
-    echo    "<th>dag</td>";
-    echo    "<th>temp</th>";
+    echo    "<th>dagen</td>";
+    echo    "<th>temperatuur</th>";
     echo "</tr>";
     echo "<tr>";
     echo    "<td>&nbsp;</td>";
@@ -34,12 +34,22 @@ $hetWeer = [
     foreach($hetWeer as $dag => $temp) {
     echo "<tr>";
     echo    "<td class=\"dag\"><span class=\"dag\">$dag</span></td>";
-    echo    "<td  class=\"temp\"><spanclass=\"temp\">$temp</span></td>";
+    echo    "<td  class=\"temp\"><spanclass=\"temp\">$temp °C</span></td>";
     echo "</tr>";
     echo "<br>";
     }
 
     ?>
     </table>
+    <?php
+
+    // echo "<br><br>";
+
+    // foreach($hetWeer as $dag2 => $temp2) {
+    //     echo "<P>Vandaag $dag2 is het $temp2 °C.</P>";
+    //     echo "<br>";
+    // }
+
+    ?>
 </body>
 </html>
